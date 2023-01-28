82e6661a5d2055e62e77226b6b978e9ef9ee2374
TLouf <loufthomas@gmail.com>
2023-01-28 18:14:51 +0100

    Only run mypy on src

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -147,7 +147,7 @@ def safety(session: Session) -> None:
 @session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["src", "tests", "docs/source/conf.py"]
+    args = session.posargs or ["src"]
     session.install(".")
     session.install("mypy", "pytest")
     session.run("mypy", *args)
