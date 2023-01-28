a0a9ef2c9f8380692cf974575e57ccc3490bf244
TLouf <loufthomas@gmail.com>
2023-01-28 17:52:57 +0100

    Add version resolution to __init__

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,11 @@
 """{{cookiecutter.friendly_name}}."""
+import sys
+
+if sys.version_info < (3, 10):
+    # compatibility for python <3.10
+    import importlib_metadata as metadata
+else:
+    from importlib import metadata
+
+
+__version__ = metadata.version("{{cookiecutter.package_name}}")
